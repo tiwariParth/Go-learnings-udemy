714c4d690c8edf808af0c601e124fd059875faa6
tiwariParth <243mjparth@gmai.com>
2025-11-02 16:34:54 +0530

    Refactor investment calculator for user input and enhance profit calculator functionality

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,16 +8,27 @@ import (
 func main() {
 	const inflationRate = 2.5
 
-	investmentAmount := 1000.0
-	years := 10.0
-	expectedReturnRate := 5.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
-	investmentAmount = 2000
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// fmt.Println(futureValue)
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Real Value: %.1f\n", futureRealValue)
+	// fmt.Printf(`Future value: %.2f
+	// Future Real value: %.2f`, futureValue, futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
 }
---
+++ profit_calculator.go
@@ -0,0 +1,32 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	fmt.Println("Calculate your profits!")
+
+// 	var revenue float64
+// 	var expenses float64
+// 	var taxRate float64
+
+// 	fmt.Print("Enter Your Revenue: ")
+// 	fmt.Scan(&revenue)
+
+// 	fmt.Print("Enter your Expenses: ")
+// 	fmt.Scan(&expenses)
+
+// 	fmt.Print("Enter your tax rate: ")
+// 	fmt.Scan(&taxRate)
+
+// 	EBT := revenue - expenses
+// 	fmt.Printf("Your earnings without taxes: %v \n", EBT)
+
+// 	profit := EBT * (1 - taxRate/100)
+// 	fmt.Printf("Your profits are: %v \n", profit)
+
+// 	ratio := EBT / profit
+// 	fmt.Printf("Your total ration: %v\n", ratio)
+
+// }
