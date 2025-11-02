1d1bff14448210c2cf85d1ca14383bfb0b6d57d2
tiwariParth <243mjparth@gmai.com>
2025-11-02 13:37:31 +0530

    Add future real value calculation to investment calculator

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
+
 	investmentAmount := 1000.0
 	years := 10.0
 	expectedReturnRate := 5.5
 
+	investmentAmount = 2000
+
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
